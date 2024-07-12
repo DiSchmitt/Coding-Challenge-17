4e993acb5097411ace6940565538cead00d029fa
Dawson Schmitt <dischmitt@usf.edu>
2024-07-12 11:28:10 -0400

    Code tasks module

--- my-app/src/tasks.js
+++ my-app/src/tasks.js
@@ -1,2 +1,11 @@
 //Tasks Module (tasks.js): Contains an array of task objects, each with properties like id, name, and isCompleted.
 
+const tasks = [
+  { id: 1, name: "Create react components", isCompleted: true },
+  { id: 2, name: "Fill out react components", isCompleted: true },
+  { id: 3, name: "Complete shift at work", isCompleted: false },
+  { id: 4, name: "Spend time with visiting cousin", isCompleted: false },
+  { id: 5, name: "Clean room", isCompleted: true },
+];
+
+export default tasks;
